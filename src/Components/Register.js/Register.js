d183897b7f87e8e2120c5d6ca85875c1acd53e21
md-rafiul <md.rafiul.pau@gmail.com>
2022-10-27 10:58:30 +0600

    login and reg added

--- src/Components/Register.js/Register.js
+++ src/Components/Register.js/Register.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const Register = () => {
-  return <div className="container">reg</div>;
+  return (
+    <div className="container">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Your name:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your name"
+            name="name"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Your Image URL:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your image url"
+            name="imgURL"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Register;
